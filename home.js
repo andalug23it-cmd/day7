f062bbf4e6a52dde7225947305a688edd2b81a83
andalug23it-cmd <andal.ug.23.it@francisxavier.ac.in>
2025-08-20 10:40:30 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="page">
+      <h1>Welcome to the Voting App 🎉</h1>
+      <p>Use the navigation above to cast your vote or view results.</p>
+    </div>
+  );
+}
+
+export default Home;
